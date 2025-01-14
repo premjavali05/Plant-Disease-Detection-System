4064bbb6245e67f6dc308e67daa1075e12498785
Prem M Javali <120392173+premjavali05@users.noreply.github.com>
2025-01-14 22:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def load_model():
     # Option 1: If using GitHub releases
     try:
         # Replace this URL with your actual model file URL
-        model_url = "MobileNetV2_plantdiseases_model.keras"
+        model_url = "https://github.com/user-attachments/files/18413817/MobileNetV2_plantdiseases_model.zip"
         response = requests.get(model_url)
         response.raise_for_status()  # Raises an HTTPError if the status is 4xx, 5xx
         
